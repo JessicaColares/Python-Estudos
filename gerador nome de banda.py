4e81521142df5cf7e7a747fde27863f80f4a6bd6
Jessica Colares <95100614+JessicaColares@users.noreply.github.com>
2022-09-15 15:16:23 -0400

    Create gerador nome de banda.py

---
+++ gerador nome de banda.py
@@ -0,0 +1,11 @@
+# 1. Create a greeting for your program.
+print("Olá, vamos criar o nome da sua banda.")
+
+# 2. Ask the user for the city that they grew up in.
+cidade = input("Diga a cidade onde você nasceu:\n")
+
+#3. Ask the user for the name of a pet.
+animal = input("Diga qual o nome do seu animal de estimaçao:\n")
+
+#4. Combine the name of their city and pet and show them their band name.
+print("Nome da sua banda:\n" + cidade + " " + animal)
