2b3a7e5500b8f0bdf7c34da2c253f7c859d89a57
Jessica Colares <95100614+JessicaColares@users.noreply.github.com>
2022-09-15 15:12:43 -0400

    Create dia 2 exercicio 2 imc.py

---
+++ dia 2 exercicio 2 imc.py
@@ -0,0 +1,14 @@
+# Descobrindo o IMC
+
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+BMI = (float(weight)/(float(height) ** 2))
+
+BMI_INT = int(BMI)
+
+print(BMI_INT)
