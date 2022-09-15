683ac104759c237ee8c8b8595516e71e43700d86
Jessica Colares <95100614+JessicaColares@users.noreply.github.com>
2022-09-15 15:11:04 -0400

    Create dia 1 exercicio 1 printing.py

---
+++ dia 1 exercicio 1 printing.py
@@ -0,0 +1,6 @@
+# Instructions
+
+# Write a program in main.py that prints the same notes from the previous lesson using what you have learnt about the Python print function.
+
+# Write your code below this line 👇
+print("Hello world!")
